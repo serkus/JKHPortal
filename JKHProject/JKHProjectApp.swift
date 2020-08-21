597bb976b0bc313f377854cc75b47ad638ce18ff
vadi2016 <vadi2016@outlook.com>
2020-08-22 00:32:58 +0300

    Initial Commit

---
+++ JKHProject/JKHProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  JKHProjectApp.swift
+//  JKHProject
+//
+//  Created by Alexey Khorikov on 22.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct JKHProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
