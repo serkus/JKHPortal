7412d4eb296932de20f7554e782f3b20590e3956
serkus01 <7erkus@gmail.com>
2020-08-22 16:53:34 +0900

    + add task

--- app/api/questionnaire/questionnaire.py
+++ app/api/questionnaire/questionnaire.py
@@ -11,24 +11,30 @@ def getQuestionnaire(request):
 	api_db.getTask(task, task)
 	return jsonify(),  20
 
-
 @questionnaire.route('/questionnaires/view/')
 def questionnaireIndex():
-	dataset = api_db.getTask(task, id)
+	dataset = api_db.getTask('task', id)
 	response={
 			"title": dataset.title,sult
 			"all_owners": dataset.owners
-			"result" : dataset.result
+			"result": dataset.result
 	}
 	return jsonify(response),  200
 
 
 @questionnaire.route('/questionnaires/<int:id>')
 def questionnaireIndex():
-	dataset = api_db.getTask(task, id)
+	dataset = api_db.getTask('task', id)
 	response ={
 			"id": dataset.id,
 			"title": dataset.titlem,
 			"qlist": dataset.itemlist,
 		}
 	return jsonify(response), 200
+
+@questionnaire.route('/questionnaires/add/<int:id>')
+def questionnaireAdd():
+	dataset = api_db.getTask('task', result)
+	 
+	response = {}
+	return 200
