fbf93381886d845b678eac13473cd743cef9351f
serkus01 <7erkus@gmail.com>
2020-08-22 15:58:38 +0900

    add response questionnaire und sqlite3 db

--- app/api/questionnaire/questionnaire.py
+++ app/api/questionnaire/questionnaire.py
@@ -1,16 +1,34 @@
 #-*- coding: UTF-8  -*-
 #!/usr/bin/env python
 
-rom flask import Blueprint, jsonify, request
-
-from app.services.BlockChein.classes import Blockchain
-
+from flask import Blueprint, jsonify, request
+from db import DB a as api_db
+r
 questionnaire = Blueprint("questionnaire", __name__)
 
 @questionnaire.route('/questionnaires', methods=['GET'])
+def getQuestionnaire(request):
+	api_db.getTask(task, task)
+	return jsonify(),  20
 
-def getQuestionnaire():
-	pass
-questionnaire.route('/questionnaires/<int:id>')
+
+@questionnaire.route('/questionnaires/view/')
 def questionnaireIndex():
-	pass
+	dataset = api_db.getTask(task, id)
+	response={
+			"title": dataset.title,sult
+			"all_owners": dataset.owners
+			"result" : dataset.result
+	}
+	return jsonify(response),  200
+
+
+@questionnaire.route('/questionnaires/<int:id>')
+def questionnaireIndex():
+	dataset = api_db.getTask(task, id)
+	response ={
+			"id": dataset.id,
+			"title": dataset.titlem,
+			"qlist": dataset.itemlist,
+		}
+	return jsonify(response), 200
